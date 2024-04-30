f120c523cd1c795adf37385de8a5161f8cf71d0d
S.Mostafa Mousavi <smousavi05@users.noreply.github.com>
2024-04-29 21:03:29 -0700

    Add files via upload

---
+++ synthetic/allfk.pl
@@ -0,0 +1,5 @@
+print "perl fk.pl -Mnmsz/12 -N2048/0.1 ";
+for ($i = 60; $i < 120; $i = $i + 10) {
+    print "$i ";
+}
+print "\n";
---
+++ synthetic/allsyn.pl
@@ -0,0 +1,3 @@
+for ($i = 60; $i < 120; $i = $i + 10) {
+    print "syn -M3.5/90/80/-70 -D0.2 -A45 -Osyn_$i.z -Gnmsz_12/$i.grn.0\n";
+}
